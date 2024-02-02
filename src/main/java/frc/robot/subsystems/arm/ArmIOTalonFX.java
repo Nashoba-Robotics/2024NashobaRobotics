7b89d4928c6b3405f8f5ced6ca1e9eea42d6579d
Yi Cheng <0418yicheng@gmail.com>
2024-02-02 13:49:28 -0500

    Oops, forgot a thing

--- src/main/java/frc/robot/subsystems/arm/ArmIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOTalonFX.java
@@ -1,20 +1,34 @@
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 
 import frc.robot.Constants;
 
 public class ArmIOTalonFX implements ArmIO{
     //2 for shooter rollers (Krakens)
     //1 for pivot (Kraken maybe)
-    TalonFX shooter, shooter2;
-    TalonFX pivot;
+    private TalonFX shooter, shooter2;
+    private TalonFX pivot;
+
+    private TalonFXConfiguration shooterConfig, pivotConfig;
 
     public ArmIOTalonFX(){
-        shooter = new TalonFX(Constants.Arm.SHOOTER_PORT, "RIO");
-        shooter2 = new TalonFX(Constants.Arm.SHOOTER_PORT_2, "RIO");
+        shooter = new TalonFX(Constants.Arm.SHOOTER_PORT, "rio");
+        shooter2 = new TalonFX(Constants.Arm.SHOOTER_PORT_2, "rio");
 
-        pivot = new TalonFX(Constants.Arm.PIVOT_PORT, "RIO");
+        pivot = new TalonFX(Constants.Arm.PIVOT_PORT, "rio");
+
+        shooterConfig = new TalonFXConfiguration();
+        shooterConfig.Audio.BeepOnBoot = true;
+        shooterConfig.Audio.BeepOnConfig = false;
+        shooterConfig.CurrentLimits.StatorCurrentLimit = 0;
+        shooterConfig.CurrentLimits.StatorCurrentLimitEnable = false;
+        shooterConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+
+
     }
 
     public void updateInputs(ArmIOInputs inputs){
