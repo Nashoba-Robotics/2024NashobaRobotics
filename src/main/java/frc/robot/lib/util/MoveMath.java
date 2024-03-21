55d27b274709020b5d1bfee2f07ac12bf0d48624
Yi Cheng <0418yicheng@gmail.com>
2024-03-21 06:04:17 -0400

    Added math. Still need to find note exit speed

---
+++ src/main/java/frc/robot/lib/util/MoveMath.java
@@ -0,0 +1,42 @@
+package frc.robot.lib.util;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.drive.DriveSubsystem;
+
+public class MoveMath {
+    public static DriveSubsystem drive = RobotContainer.drive;
+    private static final double NOTE_SPEED = 1;    //m/s
+    public static Translation3d aimToSpeaker(){
+        Translation3d speakerPos = Constants.Field.getSpeakerPos();
+        Pose2d drivePos = drive.getPose();
+        ChassisSpeeds driveSpeeds = drive.getFieldRelativeSpeeds();
+        Rotation2d robotToSpeakerAngle = Rotation2d.fromRadians(Math.atan2(
+            speakerPos.getY() - drivePos.getY(),
+            speakerPos.getX() - drivePos.getX())
+        );
+
+        double noteVelX = NOTE_SPEED*robotToSpeakerAngle.getCos();
+        double robotVelX = driveSpeeds.vxMetersPerSecond;
+
+        //Hotdog
+        double t = Math.abs(noteVelX + robotVelX)/drivePos.getX();
+        
+        double noteVelY = NOTE_SPEED * robotToSpeakerAngle.getCos();
+        double robotVelY = driveSpeeds.vyMetersPerSecond;
+
+        double yOffset = (noteVelY + robotVelY) * t;
+
+        /*
+         * targetAngle = Rotation2d.fromRadians(Math.atan2(
+            Constants.Field.getSpeakerPos().getY() - drive.getPose().getY(),
+            Constants.Field.getSpeakerPos().getX() - drive.getPose().getX()));
+         */
+        return null;
+    }
+}
