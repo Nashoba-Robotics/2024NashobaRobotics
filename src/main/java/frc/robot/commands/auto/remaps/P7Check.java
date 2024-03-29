cbdd63f69fdf5e58ef2afc07e2c70957202d92eb
Ben Brodeur <2024bbrodeur@mynrsd.com>
2024-03-29 15:03:20 -0400

    does what the branch name says

---
+++ src/main/java/frc/robot/commands/auto/remaps/P7Check.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.auto.remaps;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.RobotContainer;
+
+public class P7Check extends Command {
+    
+    @Override
+    public void initialize() {
+        if(!RobotContainer.sensors.getLoaderSensor() && !RobotContainer.sensors.getShooterSensor()) {
+            CommandScheduler.getInstance().schedule(AutoBuilder.buildAuto("NoP7"));
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
