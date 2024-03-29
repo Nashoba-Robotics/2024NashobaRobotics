c3a722f095680cff4009f977746d2cf7351792a1
Yi Cheng <0418yicheng@gmail.com>
2024-03-29 15:16:34 -0400

    _

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,7 +59,6 @@ public class Robot extends LoggedRobot {
     jank.start();
 
     RobotContainer.odometryFlag = false;
-    RobotContainer.drive.overrideVisionOdo = false;
   }
 
   @Override
@@ -203,6 +202,7 @@ public class Robot extends LoggedRobot {
     CommandScheduler.getInstance().schedule(new President());
     Governor.setRobotState(RobotState.NEUTRAL, true);
 
+    RobotContainer.drive.overrideVisionOdo = false;
 
   }
 
