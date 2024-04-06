df4d06fbb8ce915f6ba509cd9bdaca95bba7cb73
Ben Brodeur <2024bbrodeur@mynrsd.com>
2024-04-06 10:29:21 -0400

    new autos

---
+++ src/main/java/frc/robot/commands/auto/remaps/P5CheckP4.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.auto.remaps;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.RobotContainer;
+
+public class P5CheckP4 extends Command {
+    
+    @Override
+    public void initialize() {
+
+        if(!RobotContainer.sensors.getLoaderSensor() && !RobotContainer.sensors.getShooterSensor()) {
+            CommandScheduler.getInstance().schedule(AutoBuilder.buildAuto("NoP5 To P4"));
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
